ecda82dc265c501abc34d45c7ab9fad10720cf45
malak <mohamedmalak2111@gmail.com>
2023-08-26 15:24:18 +0300

    phonebook application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [ { path: 'login', component: LoginComponent },
-{ path: 'home', component: HomeComponent },];
+
+const routes: Routes = [
+ { path: 'login', component: LoginComponent },
+{ path: 'home', component: HomeComponent },
+{ path: 'header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
